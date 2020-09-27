6f5b7b890d45ebe15a7e6b4177953ca512e1f0a5
classstruct <60219182+classstruct@users.noreply.github.com>
2020-09-27 09:16:27 +0800

    Add files via upload

--- 解析.cpp
+++ 解析.cpp
@@ -1,12 +1,12 @@
-#include"tao.h"
+#include"/storage/emulated/0/0/1/tao.h"
 int main()
 {
 	char inputstr[200];
 	cout<<"求导console，请输入关于x表达式:"<<endl;
     cin>>inputstr;
-    cout<<"debug log:"<<endl;;
     char var[2]="x";
     funcs f1(inputstr,2,var);
     funcs f2=f1.singleder(var[0]);
     cout<<"derivative:"<<f2.show;
 }
+//可在tao.h中查找函数，当前支持求值，(多重)积分，(多重)导数，尚未支持不定积分
